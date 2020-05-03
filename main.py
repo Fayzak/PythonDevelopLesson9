f38a9b9a7c79000c77bce53c05d229411c8967c6
Fayzak <63008739+Fayzak@users.noreply.github.com>
2020-05-03 16:51:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -38,57 +38,62 @@ class DurakGame:
 
         if c_card[-1] == h_card[-1]:
             if int(c_card[:-1]) > int(h_card[:-1]):
-                print("Computer wins!")
+                result = "Computer wins!"
             elif int(c_card[:-1]) == int(h_card[:-1]):
-                print("Одинаковые карты. Ничья")
+                result = "Одинаковые карты. Ничья"
             else:
-                print("Human wins!")
+                result = "Human wins!"
         else:
-            print("Разные масти")
+            result = "Разные масти"
+        print(result)
+        return result
 
 
-if __name__ == "__main__":
+def gen_deck():
+    """
+    Формируем колоду
+    :return:
+    """
     deck = []
-    player_hand = {}
-    computer_hand = {}
+    for i in range(9):
+        for j in range(4):
+            _card = str(cards[i]) + lears[j]
+            deck.append(_card)
+    return deck
 
-    def gen_deck():
-        """
-        Формируем колоду
-        :return:
-        """
-        for i in range(9):
-            for j in range(4):
-                _card = str(cards[i]) + lears[j]
-                deck.append(_card)
-        return deck
 
-    gen_dec = gen_deck()
+class Hands:
+    def __init__(self, gen_dec):
+        self.gen = gen_dec
 
-    def gen_h_hand():
+    def gen_h_hand(self):
         """
         Рука игрока
         :return:
         """
+        gen_dec = self.gen
+        player_hand = {}
         for i in range(6):
             player_hand[i] = random.choice(gen_dec)
             gen_dec.remove(player_hand[i])
         print("Рука игрока: {}".format(player_hand))
         return player_hand
 
-    def gen_c_hand():
+    def gen_c_hand(self):
         """
         Рука игрока
         :return:
         """
+        gen_dec = self.gen
+        computer_hand = {}
         for i in range(6):
             computer_hand[i] = random.choice(gen_dec)
             gen_dec.remove(computer_hand[i])
         print("Рука Компьютера: {}".format(computer_hand))
         return computer_hand
 
-    # Перердаем классу игровой логики сформированные руки
-    h = gen_h_hand()
-    c = gen_c_hand()
-    g = DurakGame(h, c)
-    g.logic()
+
+gen = Hands(gen_deck())
+game = DurakGame(gen.gen_h_hand(), gen.gen_c_hand())
+if __name__ == "__main__":
+    game.logic()
